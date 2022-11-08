abf1b43ef283469a6c69fb6374b00facd022d2b2
eqram1 <abmeqramhossain@gmail.com>
2022-11-08 14:00:33 +0600

    added footer logo name with six services name

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -16,19 +16,8 @@ const Footer = () => {
                 <a href='/' className="link link-hover">Taiwind</a>
                 <a href='/' className="link link-hover">Firebase-Mongodb</a>
             </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a href='/' className="link link-hover">About us</a>
-                <a href='/' className="link link-hover">Contact</a>
-                <a href='/' className="link link-hover">Jobs</a>
-                <a href='/' className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a href='/' className="link link-hover">Terms of use</a>
-                <a href='/' className="link link-hover">Privacy policy</a>
-                <a href='/' className="link link-hover">Cookie policy</a>
-            </div>
+
+
         </footer>
     );
 };
