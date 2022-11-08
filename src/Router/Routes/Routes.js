a77e1efdc58d64402e89264fea55410cf9909040
eqram1 <abmeqramhossain@gmail.com>
2022-11-09 01:01:06 +0600

    added logout section with local host responsive

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
+import Logout from "../../pages/Logout/Logout";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/logout',
+                element: <Logout></Logout>
             }
 
         ]
---
+++ src/pages/Logout/Logout.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Logout = () => {
+    const handlelogout = event => {
+        event.preventDefault();
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Logout</h1>
+                    <p className="py-6">Please log out.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handlelogout} className="card-body">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">user info</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Service info</span>
+                            </label>
+                            <input type="text" placeholder="Service Id" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Logout</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Logout;
