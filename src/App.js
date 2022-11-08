75016de2df3f2852e4e0511178ce2c6ccffb0c99
eqram1 <abmeqramhossain@gmail.com>
2022-11-08 14:47:18 +0600

    added navbar with show maximum of 03 services

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import router from './Router/Routes/Routes';
 const App = () => {
 
   return (
-    <div className='max-w-2xl'>
-      <RouterProvider router={router}></RouterProvider>
-    </div>
+    <div>
+      < RouterProvider router={router} ></RouterProvider >
+    </div >
   );
 };
 
